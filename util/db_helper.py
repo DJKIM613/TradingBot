21b7594fb31254eb5f5c0beb69e825a64094d2bf
DJKIM613 <best_sheild97@naver.com>
2022-03-24 13:33:06 +0900

    add universe crawler

--- util/db_helper.py
+++ util/db_helper.py
@@ -1,8 +1,17 @@
 import sqlite3
 
+root_path = r"C:\Users\spong\PycharmProjects\TradingBot"
+database_path = root_path + r"\database"
+
+
+def check_database_exist(db_name):
+	with open(database_path + r'\{}.db'.format(db_name), 'w'):
+		pass
+
 
 def check_table_exist(db_name, table_name):
-	with sqlite3.connect('{}.db'.format(db_name)) as con:
+	check_database_exist(db_name)
+	with sqlite3.connect(database_path + r'\{}.db'.format(db_name)) as con:
 		cur = con.cursor()
 		sql = "SELECT name FROM sqlite_master WHERE type='table' and name=:table_name"
 		cur.execute(sql, {"table_name": table_name})
@@ -14,16 +23,18 @@ def check_table_exist(db_name, table_name):
 
 
 def insert_df_to_db(db_name, table_name, df, option="replace"):
-	with sqlite3.connect('{}.db'.format(db_name)) as con:
+	check_database_exist(db_name)
+	with sqlite3.connect(database_path + r'\{}.db'.format(db_name)) as con:
 		df.to_sql(table_name, con, if_exists=option)
 
 
 def execute_sql(db_name, sql, param={}):
-	with sqlite3.connect('{}.db'.format(db_name)) as con:
+	check_database_exist(db_name)
+	with sqlite3.connect(database_path + r'\{}.db'.format(db_name), isolation_level=None) as con:
 		cur = con.cursor()
 		cur.execute(sql, param)
 		return cur
 
 
 if __name__ == "__main__":
-	pass
+	check_database_exist('hihi')
--- util/practice_crawling.py
+++ util/practice_crawling.py
@@ -3,98 +3,80 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import os
 
-BASE_URL = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok='
-CODES = [0, 1]  # KOSPI:0, KOSDAQ:1
-START_PAGE = 1
-fields = []
-now = datetime.now()
-formattedDate = now.strftime("%Y%m%d")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+import time
 
-def execute_crawler():
-	# KOSPI, KOSDAQ 종목을 하나로 합치는데 사용할 변수
-	df_total = []
+import pathlib
 
-	# CODES에 담긴 KOSPI, KOSDAQ 종목 모두를 크롤링하기 위해 for문을 사용
-	for code in CODES:
-		# 전체 페이지 개수를 가져오기 위한 코드
-		res = requests.get(BASE_URL + str(CODES[0]))
-		page_soup = BeautifulSoup(res.text, 'lxml')
+import pandas as pd
+from datetime import datetime
 
-		# '맨뒤'에 해당하는 태그를 기준으로 전체 페이지 개수 추출하기
-		total_page_num = page_soup.select_one('td.pgRR > a')
-		total_page_num = int(total_page_num.get('href').split('=')[-1])
-
-		# 조회할 수 있는 항목정보들 추출
-		ipt_html = page_soup.select_one('div.subcnt_sise_item_top')
-
-		# 전역변수 fields에 항목들을 담아 다른 함수에서도 접근가능하도록 만듬
-		global fields
-		fields = [item.get('value') for item in ipt_html.select('input')]
-
-		# page마다 존재하는 모든 종목들의 항목정보를 크롤링해서 result에 저장(여기서 crawler 함수가 한 페이씩 크롤링해오는 역할을 담당)
-		result = [crawler(code, str(page)) for page in range(1, total_page_num + 1)]
-
-		# 전체 페이지를 저장한 result를 하나의 데이터프레임으로 만듬
-		df = pd.concat(result, axis=0, ignore_index=True)
-
-		# 변수 df는 KOSPI, KOSDAQ별로 크롤링한 종목 정보이고 이를 하나로 합치기 위해 df_total에 추가
-		df_total.append(df)
-
-	# df_total를 하나의 데이터프레임으로 만듬
-	df_total = pd.concat(df_total)
-
-	# 합친 데이터 프레임의 index 번호를 새로 매김
-	df_total.reset_index(inplace=True, drop=True)
-
-	# 전체 크롤링 결과를 엑셀 출력
-	df_total.to_excel('NaverFinance.xlsx')
-
-	# 크롤링 결과를 반환
-	return df_total
+from db_helper import *
 
 
-def crawler(code, page):
-	global fields
+def set_chrome_driver(download_path=None):
+	chrome_options = webdriver.ChromeOptions()
+	print(download_path)
+	if download_path is not None:
+		prefs = {}
+		prefs["download.default_directory"] = download_path
+		chrome_options.add_experimental_option("prefs", prefs)
 
-	# Naver fimance에 전달할 값들 세팅(요청을 보낼 때는 menu, fieldIds, returnUrl을 지정해서 보내야 함)
-	data = {'menu': 'market_sum',
-	        'fieldIds': fields,
-	        'returnUrl': BASE_URL + str(code) + "&page=" + str(page)}
+	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+	return driver
 
-	# 네이버로 요청을 전달(post방식)
-	res = requests.post('https://finance.naver.com/sise/field_submit.nhn', data=data)
 
-	page_soup = BeautifulSoup(res.text, 'lxml')
+def wait_element(driver, path, timeout=10):
+	WebDriverWait(driver, timeout).until(
+		EC.presence_of_element_located((By.XPATH, path))
+	)
+	WebDriverWait(driver, timeout).until(
+		EC.element_to_be_clickable((By.XPATH, path))
+	)
 
-	# 크롤링할 table의 html 가져오는 코드(크롤링 대상 요소의 클래스는 브라우저에서 확인)
-	table_html = page_soup.select_one('div.box_type_l')
 
-	# column명을 가공
-	header_data = [item.get_text().strip() for item in table_html.select('thead th')][1:-1]
+def crawlUniverse():
+	cwd = os.getcwd()
+	driver = set_chrome_driver(cwd)
+	url_main = "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020203"
+	driver.get(url_main)
 
-	# 종목명 + 수치 추출 (a.title = 종목명, td.number = 기타 수치)
-	inner_data = [item.get_text().strip() for item in table_html.find_all(lambda x:
-	                                                                      (x.name == 'a' and
-	                                                                       'tltle' in x.get('class', [])) or
-	                                                                      (x.name == 'td' and
-	                                                                       'number' in x.get('class', []))
-	                                                                      )]
+	path_sebu = '//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[5]/a'
+	path_detail = '//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[5]/ul/li[2]/a'
+	path_data = '//*[@id="jsMdiContent"]/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/table/tbody/tr[1]/td[1]'
+	path_download = '//*[@id="MDCSTAT035_FORM"]/div[3]/div/p[2]/button[2]'
+	path_csv = '//*[@id="ui-id-1"]/div/div[2]/a'
 
-	# page마다 있는 종목의 순번 가져오기
-	no_data = [item.get_text().strip() for item in table_html.select('td.no')]
-	number_data = np.array(inner_data)
+	paths = [(path_sebu, path_detail), (path_detail, path_data), (path_download, path_csv), (path_csv, None)]
 
-	# 가로 x 세로 크기에 맞게 행렬화
-	number_data.resize(len(no_data), len(header_data))
+	wait_element(driver, path_sebu)
+	for i in range(len(paths)):
+		cur = driver.find_element(By.XPATH, paths[i][0])
+		cur.click()
+		if paths[i][1] is not None:
+			wait_element(driver, paths[i][1])
 
-	# 한 페이지에서 얻은 정보를 모아 DataFrame로 만들어 반환
-	df = pd.DataFrame(data=number_data, columns=header_data)
-	return df
+		else:
+			time.sleep(10)
+
+	for name in pathlib.Path(cwd).glob('*.csv'):
+		df = pd.read_csv(name, encoding='cp949')
+		date = datetime.today().strftime("%Y%m%d")
+
+		insert_df_to_db('universe', date, df)
+
+		os.remove(name)
+
+	time.sleep(2000)
 
 
 if __name__ == "__main__":
-	print('Start!')
-	execute_crawler()
-	print('End')
+	crawlUniverse()
